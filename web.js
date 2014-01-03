b597f9b12854bcbfee6dff67324cf8b82b3fa36b
Michael Klopf <klopf.michael@gmail.com>
2014-01-03 13:03:12 +0100

    Added stylus stylesheet and adapted web.js to process styl to css.

--- web.js
+++ web.js
@@ -4,28 +4,18 @@ var sys = require('util');
 var rest = require('restler');
 var fs = require('fs');
 var jade = require('jade');
+var stylus = require('stylus');
 
 var app = express();
 
+app.use(stylus.middleware(__dirname + '/static'));
 app.use(express.static(__dirname + '/static'));
 app.use(express.bodyParser());
 
 app.get('/', function(req, res) {
-    //var data = fs.readFileSync('index.html').toString();
     var data = jade.renderFile(__dirname + '/index.jade');
+    res.contentType('text/html');
     res.send(200, data);
-    //res.contentType('text/html');
-    //res.send(200, 'We learn express');
-    /*
-     // example for consuming another api
-     rest.get('url').on('complete', function(response) {
-        if (response instanceof Error) {
-            sys.put('Error: ' + response.message);
-        } else {
-            sys.put(response);
-        }
-     });
-    */
 });
 
 app.post('/testpost', function(req, res) {
@@ -37,3 +27,14 @@ var port = 3000;
 http.createServer(app).listen(port, function() {
     console.log('Listening on port ' + port);
 });
+
+/*
+ // example for consuming another api
+ rest.get('url').on('complete', function(response) {
+    if (response instanceof Error) {
+        sys.put('Error: ' + response.message);
+    } else {
+        sys.put(response);
+    }
+ });
+*/
